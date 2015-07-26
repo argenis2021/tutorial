41dd24b029b55689008fd9362acb65234a711ed8
Argenis <argenismoncada@gmail.com>
2015-07-25 20:19:09 -0430

    bug in line 10

--- var.py
+++ var.py
@@ -4,7 +4,5 @@ i = 5
 print i
 i = i + 1
 print i
-s = '''This is a multi-line string.
-This is the second line.'''
+s = '''This is a multi-line string. This is the second line.'''
 print s
-Output:
