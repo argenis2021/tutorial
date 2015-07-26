e012fd6ed6a007fd318a263ae2c252382a72a8e7
Argenis <argenismoncada@gmail.com>
2015-07-25 20:13:33 -0430

    My second commit

---
+++ var.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# Using this line the computer know this is a python executable file
+i = 5
+print i
+i = i + 1
+print i
+s = '''This is a multi-line string.
+This is the second line.'''
+print s
+Output:
